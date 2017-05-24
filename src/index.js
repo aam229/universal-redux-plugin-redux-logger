1e26f082fa857f51decd26d0aba26f08d9369b78
Alexandre Massonneau <alexandre.massonneau@webcbg.com>
2017-05-24 14:54:35 -0400

    Fix import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { hooks, environments, positions, register } from 'universal-redux';
 
 register(hooks.CREATE_REDUX_MIDDLEWARE, ({ middleware, ...other }) => {
